3215b22bad6a5585414b6490a58f01b499fcb4fa
Fabio Spampinato <spampinabio@gmail.com>
2019-08-27 20:05:10 +0200

    Added a test for function values

--- test/index.js
+++ test/index.js
@@ -8,10 +8,21 @@ import {default as resolveTimeout} from '../dist';
 
 describe ( 'Promise Resolve Timeout', it => {
 
-  it ( 'works', async t => {
+  it ( 'works with primitive values', async t => {
 
     const start = Date.now (),
           value = await resolveTimeout ( 1000, 123 ),
+          elapsed = Date.now () - start;
+
+    t.true ( elapsed >= 1000 );
+    t.is ( value, 123 );
+
+  });
+
+  it ( 'works with function values', async t => {
+
+    const start = Date.now (),
+          value = await resolveTimeout ( 1000, () => 123 ),
           elapsed = Date.now () - start;
 
     t.true ( elapsed >= 1000 );
